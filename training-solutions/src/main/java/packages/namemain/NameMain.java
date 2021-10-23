1ec30b5cb221f49b0f1ae550f71fe4d0bc76c997
Morsiani Renato <m.renato@freemail.hu>
2021-10-23 16:56:39 +0200

    Packages task done

---
+++ training-solutions/src/main/java/packages/namemain/NameMain.java
@@ -0,0 +1,16 @@
+package packages.namemain;
+
+import packages.firstname.FirstName;
+import packages.lastname.LastName;
+import packages.prefix.Prefix;
+
+public class NameMain {
+
+    public static void main(String[] args) {
+        Prefix prefix = new Prefix("Dr.");
+        FirstName firstName = new FirstName("John");
+        LastName lastName = new LastName("Doe");
+
+        System.out.println("I'm " + prefix.getPre() + " " + firstName.getFirst() + " " + lastName.getLast());
+    }
+}
