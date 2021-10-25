892f1374927eeeb226b269b489d9fdec3bf012c5
Morsiani Renato <m.renato@freemail.hu>
2021-10-25 21:17:34 +0200

    Debug task debugged with joy

--- training-solutions/src/main/java/debug/Debug.java
+++ training-solutions/src/main/java/debug/Debug.java
@@ -12,13 +12,13 @@ public class Debug {
         fruits.set(3, peach);
         for (int i = 0; i < fruits.size(); i++) {
             String fruit = fruits.get(i);
-            fruits.contains(fruit.substring(0, 2));
+            fruits.set(i, fruit.substring(0, 2));
         }
-        System.out.println(fruits.get(1));
+        System.out.println(fruits.get(2));
 
         int[] numbers = {3, 7, -2, 1, -4, 5};
 
-        numbers[2] = 0;
+        numbers[2] = -numbers.length * 2 + 2;
         for (int i = 0; i < numbers.length; i++) {
             numbers[2]++;
             numbers[2]++;
