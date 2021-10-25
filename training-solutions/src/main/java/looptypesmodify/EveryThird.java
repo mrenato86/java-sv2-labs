563bdaa837db95e5a8077ffe4a8c55d278a12156
Morsiani Renato <m.renato@freemail.hu>
2021-10-25 14:34:45 +0200

    LoopTypesModify/EveryThird class updated to better suit the task wording

--- training-solutions/src/main/java/looptypesmodify/EveryThird.java
+++ training-solutions/src/main/java/looptypesmodify/EveryThird.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class EveryThird {
 
     public int[] changeToZero(int[] numbers) {
-        int[] result = Arrays.copyOf(numbers, numbers.length);
+        int[] result = numbers; //int[] result = Arrays.copyOf(numbers, numbers.length);
         for (int i = result.length - 1; i >= 0; i -= 3) {
             result[i] = 0;
         }
@@ -14,10 +14,11 @@ public class EveryThird {
 
     public static void main(String[] args) {
         EveryThird everyThird = new EveryThird();
-        int[] data = {2, 6, 3, 5, 7, 2, 6, 2, 3, 5, 7, 3, 2, 9};
-        int[] result = everyThird.changeToZero(data);
+        //int[] data = {2, 6, 3, 5, 7, 2, 6, 2, 3, 5, 7, 3, 2, 9};
+        //int[] result = everyThird.changeToZero(data);
+        int[] result = everyThird.changeToZero(new int[]{2, 6, 3, 5, 7, 2, 6, 2, 3, 5, 7, 3, 2, 9});
 
-        System.out.println(Arrays.toString(data));
+        //System.out.println(Arrays.toString(data));
         System.out.println(Arrays.toString(result));
     }
 
