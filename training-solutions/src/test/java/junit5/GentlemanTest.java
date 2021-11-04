9a55a67ea0eb453edb11653c8b414ec30610b12e
Morsiani Renato <m.renato@freemail.hu>
2021-11-04 02:59:00 +0100

    Junit5 task done

---
+++ training-solutions/src/test/java/junit5/GentlemanTest.java
@@ -0,0 +1,30 @@
+package junit5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GentlemanTest {
+
+    @Test
+    void testSayHello() {
+        //Given
+        Gentleman gentleman = new Gentleman();
+        String name = "John Doe";
+        //When
+        String greeting = gentleman.sayHello(name);
+        //Then
+        assertEquals("Hello John Doe", greeting);
+    }
+
+    @Test
+    void testSayHelloNull() {
+        //Given
+        Gentleman gentleman = new Gentleman();
+        //When
+        String greeting = gentleman.sayHello(null);
+        //Then
+        assertEquals("Hello Anonymous", greeting);
+    }
+
+}
