49eee8325df4567e35b56c567413265c687d37f2
Morsiani Renato <m.renato@freemail.hu>
2021-11-14 23:55:27 +0100

    IntroExceptionMoreExceptions task done

---
+++ training-solutions/src/main/java/introexceptionmoreexceptions/Person.java
@@ -0,0 +1,35 @@
+package introexceptionmoreexceptions;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person(null, 32);
+        System.out.println(person);
+        System.out.println(person.getName());
+        //Exception: System.out.println(person.getName().toUpperCase());
+    }
+}
