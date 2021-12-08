91f588d2fa1d91d35014a8f0d4282c396d91a412
Morsiani Renato <m.renato@freemail.hu>
2021-12-08 15:31:00 +0100

    Interfaces task done

---
+++ training-solutions/src/main/java/interfaces/simpletasks/SimpleTasks.java
@@ -0,0 +1,33 @@
+package interfaces.simpletasks;
+
+import java.util.List;
+
+public class SimpleTasks implements Runnable {
+
+    private List<String> tasks;
+
+    public SimpleTasks(List<String> tasks) {
+        this.tasks = tasks;
+    }
+
+    public List<String> getTasks() {
+        return tasks;
+    }
+
+    private boolean nextStep() {
+        if (tasks == null || tasks.isEmpty()) {
+            return false;
+        }
+        tasks.remove(tasks.size() - 1);
+        return !tasks.isEmpty();
+    }
+
+    @Override
+    public void run() {
+        boolean next = true;
+        while (next) {
+            next = nextStep();
+        }
+        
+    }
+}
